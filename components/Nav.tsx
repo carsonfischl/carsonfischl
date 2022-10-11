2a3e25f544ab4aa0a25d6cb86f974042cdb76ea8
carsonfischl <carson.fischl@carleton.ca>
2022-10-11 12:47:39 -0400

    gradient bg

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Nav = () => {
   return (
     <div>
-        <nav className='w-100 h-24 flex bg-slate-400 align-middle text-align'>
+        <nav className='bg-gradient-to-r from-slate-500 to-slate-900 w-100 h-24 flex align-middle text-align'>
             <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                <a className='p-6 rounded-md hover:bg-slate-600' href="/">Home</a>
-                <a className='p-6 rounded-md hover:bg-slate-600' href="/about">About</a>
+                <a className='py-2 px-6 rounded-md hover:bg-white hover:text-black text-white' href="/">Home</a>
+                <a className='py-2 px-6 rounded-md hover:bg-slate-600 text-white' href="/about">About</a>
             </div>
             <div className='container flex flex-wrap justify-between items-center mx-auto'>
                 Title
             </div>
             <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                <a className='p-6 rounded-md hover:bg-slate-600' href="/login">Login</a>
-                <a className='p-6 rounded-md hover:bg-slate-600' href="/register">Register</a>
+                <a className='py-2 px-6 rounded-md hover:bg-slate-600 text-white' href="/login">Login</a>
+                <a className='py-2 px-6 rounded-md hover:bg-slate-600 text-white' href="/register">Register</a>
             </div>
         </nav>
     </div>
