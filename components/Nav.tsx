c15d024e3f9a1ac9ca76e0efe65f7abf638f90b5
carsonfischl <carson.fischl@carleton.ca>
2022-10-18 16:59:33 -0400

    made links pink to match title

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -8,17 +8,17 @@ const Nav = () => {
     <div>
         <nav className='w-screen h-40 flex align-middle text-align items-center fixed top-0'>
             <div className='container flex flex-wrap items-center mx-auto p-2 justify-center'>
-                <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/">Home</a>
-                <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/login">Projects</a>
-                <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/register">Work</a>
+                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/">Home</a>
+                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/login">Projects</a>
+                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/register">Work</a>
             </div>
-                <div className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+                <div className="before:block before:absolute before:-inset-3 before:-skew-y-3 before:bg-pink-500 relative inline-block">
                     <div className="relative text-white text-3xl">Carson S. Fischl</div>
                 </div>
             <div className='container flex flex-wrap items-center mx-auto p-2 justify-center'>
-                <a className='p-2 rounded-md hover:bg-slate-600 font-mono text-white mx-6' href="https://www.linkedin.com/in/carsonfischl/"><AiOutlineLinkedin size={30}/></a>
-                <a className='p-2 rounded-md hover:bg-slate-600 font-mono text-white mx-6' href="https://github.com/carsonfischl"><AiFillGithub size={30}/></a>
-                <a className='p-2 rounded-md hover:bg-slate-600 font-mono text-white mx-6' href="https://www.instagram.com/carson6f/"><FiInstagram size={27}/></a>
+                <a className='p-2 rounded-md hover:bg-pink-500 font-mono text-white mx-6' href="https://www.linkedin.com/in/carsonfischl/"><AiOutlineLinkedin size={30}/></a>
+                <a className='p-2 rounded-md hover:bg-pink-500 font-mono text-white mx-6' href="https://github.com/carsonfischl"><AiFillGithub size={30}/></a>
+                <a className='p-2 rounded-md hover:bg-pink-500 font-mono text-white mx-6' href="https://www.instagram.com/carson6f/"><FiInstagram size={27}/></a>
             </div>
         </nav>
     </div>
