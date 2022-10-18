689feafd4130d6e2f486defd52c17d2a771bac7f
carsonfischl <carson.fischl@carleton.ca>
2022-10-18 10:54:37 -0400

    changed navbar to mono

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Nav = () => {
   return (
     <div>
-        <nav className='bg-gradient-to-r from-slate-500 to-slate-900 w-100 h-24 flex align-middle text-align'>
-            <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                <a className='py-2 px-6 rounded-md hover:bg-white hover:text-black text-white' href="/">Home</a>
-                <a className='py-2 px-6 rounded-md hover:bg-slate-600 text-white' href="/about">About</a>
+        <nav className='bg-gradient-to-r from-slate-500 to-slate-900 w-100 h-24 flex align-middle text-align items-center'>
+            <div className='container flex flex-wrap items-center mx-auto p-2 justify-center'>
+                <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/">Home</a>
+                <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/about">About</a>
             </div>
-            <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                Title
+            <div className='container flex flex-wrap items-center mx-auto justify-center'>
+                <h1 className='subpixel-antialiased text-xl font-mono text-white'>Title</h1>
             </div>
-            <div className='container flex flex-wrap justify-between items-center mx-auto'>
-                <a className='py-2 px-6 rounded-md hover:bg-slate-600 text-white' href="/login">Login</a>
-                <a className='py-2 px-6 rounded-md hover:bg-slate-600 text-white' href="/register">Register</a>
+            <div className='container flex flex-wrap items-center mx-auto p-2 justify-center'>
+                <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/login">Login</a>
+                <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/register">Register</a>
             </div>
         </nav>
     </div>
