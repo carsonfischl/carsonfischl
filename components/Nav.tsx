70f0dcaf2696ad33cc6692e6c45ba9ff4405164f
carsonfischl <carson.fischl@carleton.ca>
2022-10-18 16:49:09 -0400

    new title, pink

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,17 +6,17 @@ import { AiOutlineLinkedin } from 'react-icons/ai';
 const Nav = () => {
   return (
     <div>
-        <nav className='w-screen h-24 flex align-middle text-align items-center fixed top-0'>
+        <nav className='w-screen h-40 flex align-middle text-align items-center fixed top-0'>
             <div className='container flex flex-wrap items-center mx-auto p-2 justify-center'>
                 <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/">Home</a>
                 <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/login">Projects</a>
                 <a className='py-2 px-6 rounded-md hover:bg-slate-600 font-mono text-white mx-2' href="/register">Work</a>
             </div>
-            <div className='container flex flex-wrap items-center mx-auto justify-center'>
-                <h1 className='subpixel-antialiased text-3xl font-mono text-white'>Carson S. Fischl</h1>
-            </div>
+                <div className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+                    <div className="relative text-white text-3xl">Carson S. Fischl</div>
+                </div>
             <div className='container flex flex-wrap items-center mx-auto p-2 justify-center'>
-            <a className='p-2 rounded-md hover:bg-slate-600 font-mono text-white mx-6' href="https://www.linkedin.com/in/carsonfischl/"><AiOutlineLinkedin size={30}/></a>
+                <a className='p-2 rounded-md hover:bg-slate-600 font-mono text-white mx-6' href="https://www.linkedin.com/in/carsonfischl/"><AiOutlineLinkedin size={30}/></a>
                 <a className='p-2 rounded-md hover:bg-slate-600 font-mono text-white mx-6' href="https://github.com/carsonfischl"><AiFillGithub size={30}/></a>
                 <a className='p-2 rounded-md hover:bg-slate-600 font-mono text-white mx-6' href="https://www.instagram.com/carson6f/"><FiInstagram size={27}/></a>
             </div>
