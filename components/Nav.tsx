32be780cd2d30349b397d15973552e4f2fb1f735
carsonfischl <carson.fischl@carleton.ca>
2022-10-21 23:20:02 -0400

    bubbles

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,15 +6,15 @@ import { AiOutlineLinkedin } from 'react-icons/ai';
 const Nav = () => {
   return (
     <div>
-        <nav className='w-screen h-40 flex align-middle text-align items-center fixed top-0'>
+        <nav className='w-screen h-40 flex align-middle text-align items-center fixed top-0 z-10'>
             <div className='container flex flex-wrap items-center mx-auto p-2 justify-center'>
-                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/">Home</a>
-                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/login">Projects</a>
-                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/register">Work</a>
+                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/work">Work</a>
+                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/projects">Projects</a>
+                <a className='py-2 px-6 rounded-md hover:bg-pink-500 font-mono text-white mx-2' href="/blog">Blog</a>
             </div>
-                <div className="before:block before:absolute before:-inset-3 before:-skew-y-3 before:bg-pink-500 relative inline-block">
-                    <div className="relative text-white text-3xl">Carson S. Fischl</div>
-                </div>
+                <a className="before:block before:absolute before:-inset-3 before:-skew-y-3 before:bg-pink-500 relative inline-block" href='/'>
+                    <div className="relative text-white text-3xl">Carson Fischl</div>
+                </a>
             <div className='container flex flex-wrap items-center mx-auto p-2 justify-center'>
                 <a className='p-2 rounded-md hover:bg-pink-500 font-mono text-white mx-6' href="https://www.linkedin.com/in/carsonfischl/"><AiOutlineLinkedin size={30}/></a>
                 <a className='p-2 rounded-md hover:bg-pink-500 font-mono text-white mx-6' href="https://github.com/carsonfischl"><AiFillGithub size={30}/></a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Nav from '../components/Nav'
+import Bubbles from '../components/Bubbles'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       </Head>
       <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono'>
         <Nav />
+        <Bubbles />
       </div>
     </div>
   )
