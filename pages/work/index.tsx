5518ff4d8fe72b618a26ab3afb729959fa33fff5
carsonfischl <carson.fischl@carleton.ca>
2022-10-23 01:59:40 -0400

    brief job ui

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -4,6 +4,9 @@ import Bubbles from '../../components/Bubbles'
 import Meta from '../../components/Meta'
 import Job from '../../components/Job'
 import { jobs } from '../../public/json/jobs.js'
+import gov from '../../public/images/gov.png'
+import hospital from '../../public/images/hospital.png'
+import snps from '../../public/images/snps.png'
 
 
 type Props = {}
@@ -14,17 +17,20 @@ const index = (props: Props) => {
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
         <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono fixed'>
         <Nav />
-        <div className='flex flex-row m-6'>
-        {jobs.map((job) => (
-            <Job
-                key={job.id}
-                title={job.title}
-                company={job.company}
-
-                start={job.date}
-
-            />
-        ))}
+        <div className='container mx-auto relative pt-12 grid grid-cols-3'>
+            <div className='flex flex-col'>
+                <img src='/images/snps.png' alt="snps" className='rounded-full ring-pink-500 w-30 h-30'/>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quo pariatur dolores veritatis libero reprehenderit, dignissimos accusamus nisi, ipsum praesentium doloremque minima ipsa officia culpa architecto deserunt similique ut ullam.</p>
+                <img src='/images/snps.png' alt="snps" className='rounded-full ring-pink-500 w-30 h-30'/>
+            </div>
+            <div className='flex flex-col'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, id temporibus deserunt iusto maxime tempora blanditiis voluptas totam modi quis eius commodi. Saepe distinctio ad, cumque amet tempora libero blanditiis.</p>
+                <img src='/images/snps.png' alt="snps" className='rounded-full ring-pink-500 w-30 h-30'/>
+            </div>
+            <div className='flex flex-col'>
+            <img src='/images/snps.png' alt="snps" className='rounded-full ring-pink-500 w-30 h-30'/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo totam cum dignissimos voluptates sint ipsum vel, autem ratione obcaecati fuga. Sequi laudantium aut mollitia minima minus, at illo ducimus reprehenderit.</p>
+            </div>
         </div>
         </div>
     </>
