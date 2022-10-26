4dcf18274396363b285ec72eae37094c0d280a6b
carsonfischl <carson.fischl@carleton.ca>
2022-10-26 00:02:33 -0400

    synchronized backgrounds

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -9,8 +9,15 @@ const index = (props: Props) => {
   return (
     <>
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
-        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono'>
+        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono fixed'>
             <Nav />
+            <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100'>
+                <div className="grid grid-cols-3 gap-6">
+                    <div className=''>01</div>
+                    <div className=''>02</div>
+                    <div className=''>03</div>
+                </div>
+            </div>
         </div>
         <Bubbles />
     </>
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -15,7 +15,7 @@ const index = (props: Props) => {
   return (
     <>
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
-        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono fixed overflow-auto'>
+        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono overflow-auto overscroll-auto'>
         <Nav />
             <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100'>
                 <div className="grid grid-cols-3 gap-6">
