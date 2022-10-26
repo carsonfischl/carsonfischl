a5adf5975a2e8610a1453bbea85477bcd198add8
carsonfischl <carson.fischl@carleton.ca>
2022-10-25 23:50:53 -0400

    unfixed bg

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -9,7 +9,7 @@ const index = (props: Props) => {
   return (
     <>
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
-        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono fixed'>
+        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono'>
             <Nav />
         </div>
         <Bubbles />
