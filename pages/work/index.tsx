b10ec05e7c5b845a8a2ae68a6a7aba487c77dd1b
carsonfischl <carson.fischl@carleton.ca>
2022-10-25 23:16:09 -0400

    finished preliminary work

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -7,6 +7,7 @@ import { jobs } from '../../public/json/jobs.js'
 import gov from '../../public/images/gov.png'
 import hospital from '../../public/images/hospital.png'
 import snps from '../../public/images/snps.png'
+import Image from 'next/image'
 
 
 type Props = {}
@@ -15,34 +16,54 @@ const index = (props: Props) => {
   return (
     <>
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
-        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono fixed'>
+        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono fixed overflow-auto'>
         <Nav />
-            <div className='align-middle justify-center items-center flex-none justify-items-center w-4/5 m-auto overflow-hidden h-100'>
+            <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100'>
                 <div className="grid grid-cols-3 gap-6">
+                    <div className=''>
+                        <Image src={snps} alt="synopsys" width={500} height={500} className='rounded-full'/>
+                    </div>
+                    <div className=''>
+                        <Image src={gov} alt="Government of Canada" width={500} height={500} className='rounded-full'/>
+                    </div>
+                    <div className=''>
+                        <Image src={hospital} alt="ohri" width={500} height={500} className='rounded-full'/>
+                    </div>
                     <div className='text-center'>
                         <h1 className='text-4xl text-white font-bold font-mono p-4'>Engineering Intern</h1>
-                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Synopsys Inc.</h1>
-                        <h3 className='text-2xl text-white font-mono p-4'>October 2021 - September 2022</h3>
                     </div>
                     <div className='text-center'>
                         <h1 className='text-4xl text-white font-bold font-mono p-4'>Data Analyst</h1>
-                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Government of Canada</h1>
-                        <h3 className='text-2xl text-white font-mono p-4'>March 2021 - August 2021</h3>
                     </div>
                     <div className='text-center'>
                         <h1 className='text-4xl text-white font-bold font-mono p-4'>Bioinformatics Intern</h1>
+                    </div>
+                    <div className='text-center'>
+                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Synopsys Inc.</h1>
+                    </div>
+                    <div className='text-center'>
+                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Government of Canada</h1>
+                    </div>
+                    <div className='text-center'>
                         <h1 className='text-4xl text-white font-bold font-mono p-4'>Ottawa Hospital Research Institute</h1>
+                    </div>
+                    <div className='text-center'>
+                        <h3 className='text-2xl text-white font-mono p-4'>October 2021 - September 2022</h3>
+                    </div>
+                    <div className='text-center'>
+                        <h3 className='text-2xl text-white font-mono p-4'>March 2021 - August 2021</h3>
+                    </div>
+                    <div className='text-center'>
                         <h3 className='text-2xl text-white font-mono p-4'>May 2020 - August 2020</h3>
                     </div>
-                    <div>04</div>
-                    <div>05</div>
-                    <div>06</div>
                     <div className='text-left'>
                         <ul className='text-white font-mono p-4'>
                             <li>• Simulated and debugged DDR PHY releases for production chips for major industry
 clients to mitigate schedule risk.</li>
+                            <br></br>
                             <li>• Implemented swizzling features in LPDDR4/3 PHYs to expedite customer
 implementation in their SoC designs.</li>
+                            <br></br>
                             <li>• Contributed to building a complex Jira-Wrike ticket synchronization script to expedite
 internal workflows and assist in long term project planning.</li>
                         </ul>
@@ -52,9 +73,11 @@ internal workflows and assist in long term project planning.</li>
                             <li>• Was at the forefront of stakeholder outreach for Canada's COVID-19 testing program,
 dealing with small businesses, NGOs, and federal, provincial and municipal government
 officials.</li>
+                            <br></br>
                             <li>• Implemented a data capture pipeline for COVID-19 testing data in Canadian
 workplaces, allowing the Deputy Minister's office to view case numbers and track
 contagion across geographic areas.</li>
+                            <br></br>
                             <li>• Oversaw UAT, data validation, and data analysis on large datasets to assure data
 integrity and successful data pipelining from a public frontend.</li>
                         </ul>
@@ -62,8 +85,10 @@ integrity and successful data pipelining from a public frontend.</li>
                     <div className='text-left'>
                         <ul className='text-white font-mono p-4'>
                             <li> Worked with a multi-disciplinary team to develop a data analysis pipeline for bulk RNA-seq data for Genome Canada's Genomic Applications Partnership Program (GAPP).</li>
+                            <br></br>
                             <li>• Used a high performance CentOS cluster to schedule and test fusion callers using bash
 and R.</li>
+                            <br></br>
                             <li>• Assisted in presenting an RNA-seq seminar to academics.</li>
                         </ul>
                     </div>
