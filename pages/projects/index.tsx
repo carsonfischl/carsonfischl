614b9e9e014af054a4bd31003a5039c63f60544f
carsonfischl <carson.fischl@carleton.ca>
2022-10-26 01:56:57 -0400

    draft site

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Nav from '../../components/Nav'
 import Bubbles from '../../components/Bubbles'
 import Meta from '../../components/Meta'
+import cc from '../../public/images/cc.png'
+import weather from '../../public/images/weather.png'
+import app from '../../public/images/app.png'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -9,13 +13,34 @@ const index = (props: Props) => {
   return (
     <>
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
-        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono fixed'>
+        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono'>
             <Nav />
             <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100'>
-                <div className="grid grid-cols-3 gap-6">
-                    <div className=''>01</div>
-                    <div className=''>02</div>
-                    <div className=''>03</div>
+                <div className="grid grid-cols-3 gap-6 py-8">
+                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
+                        <h1 className='italic font-mono text-white text-4xl my-4'>Comitas Collection</h1>
+                        <h3 className='italic font-mono text-white text-2xl my-4'>A Global Historiography through Numismatics</h3>
+                        <a className='py-2 px-6 rounded-md hover:bg-pink-500 active:bg-pink-500 font-mono text-white mx-2 my-8' href="https://comitas-collection.vercel.app">Under Construction</a>
+                    </div>
+                    <div className=''>
+                        <Image src={cc} alt='Comitas Collection' className='rounded'/>
+                    </div>
+                    <div className=''>
+                        <Image src={app} alt='personal app' className='rounded'/>
+                    </div>
+                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
+                        <h1 className='italic font-mono text-white text-4xl'>Personal App</h1>
+                        <h3 className='italic font-mono text-white text-2xl'>Some Personal Propagandizing</h3>
+                        <a className='py-2 px-6 rounded-md hover:bg-pink-500 active:bg-pink-500 font-mono text-white mx-2' href="https://play.google.com/store/apps/details?id=com.carson.app">Under Construction</a>
+                    </div>
+                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
+                        <h1 className='italic font-mono text-white text-4xl'>WeatherMe-LTE</h1>
+                        <h3 className='italic font-mono text-white text-2xl'>A Hardware Hack for a Mobile Weather Station</h3>
+                        <a className='py-2 px-6 rounded-md hover:bg-pink-500 active:bg-pink-500 font-mono text-white mx-2' href="https://cuhacking2020.tauheed-elahee.com/">The Remaining Site</a>
+                    </div>
+                    <div className=''>
+                        <Image src={weather} alt='WeatherMe-LTE' className='rounded'/>
+                    </div>
                 </div>
             </div>
         </div>
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -18,45 +18,19 @@ const index = (props: Props) => {
         <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono static'>
         <Nav />
             <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100'>
-                <div className="grid grid-cols-3 gap-6">
+                <div className="grid grid-cols-3 gap-8 py-8">
+                    <div className='col-span-3'>
+                        <div className='text-center'>
+                            <h1 className='text-4xl text-white font-bold font-mono p-4'>Synopsys Inc.</h1>
+                            <h1 className='text-4xl text-white font-bold font-mono p-4'>Engineering Intern</h1>
+                            <h3 className='text-2xl text-white font-mono p-4'>October 2021 - September 2022</h3>
+                        </div>
+                    </div>
                     <div className=''>
                         <Image src={snps} alt="synopsys" width={500} height={500} className='rounded-full'/>
                     </div>
-                    <div className=''>
-                        <Image src={gov} alt="Government of Canada" width={500} height={500} className='rounded-full'/>
-                    </div>
-                    <div className=''>
-                        <Image src={hospital} alt="ohri" width={500} height={500} className='rounded-full'/>
-                    </div>
-                    <div className='text-center'>
-                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Engineering Intern</h1>
-                    </div>
-                    <div className='text-center'>
-                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Data Analyst</h1>
-                    </div>
-                    <div className='text-center'>
-                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Bioinformatics Intern</h1>
-                    </div>
-                    <div className='text-center'>
-                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Synopsys Inc.</h1>
-                    </div>
-                    <div className='text-center'>
-                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Government of Canada</h1>
-                    </div>
-                    <div className='text-center'>
-                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Ottawa Hospital Research Institute</h1>
-                    </div>
-                    <div className='text-center'>
-                        <h3 className='text-2xl text-white font-mono p-4'>October 2021 - September 2022</h3>
-                    </div>
-                    <div className='text-center'>
-                        <h3 className='text-2xl text-white font-mono p-4'>March 2021 - August 2021</h3>
-                    </div>
-                    <div className='text-center'>
-                        <h3 className='text-2xl text-white font-mono p-4'>May 2020 - August 2020</h3>
-                    </div>
-                    <div className='text-left'>
-                        <ul className='text-white font-mono p-4'>
+                    <div className='text-center col-span-2 justify-center align-middle m-auto'>
+                        <ul className='text-white font-mono p-4 text-left'>
                             <li>• Simulated and debugged DDR PHY releases for production chips for major industry
 clients to mitigate schedule risk.</li>
                             <br></br>
@@ -67,8 +41,15 @@ implementation in their SoC designs.</li>
 internal workflows and assist in long term project planning.</li>
                         </ul>
                     </div>
-                    <div className='text-left'>
-                        <ul className='text-white font-mono p-4'>
+                    <div className='col-span-3'>
+                        <div className='text-center'>
+                            <h1 className='text-4xl text-white font-bold font-mono p-4'>Government of Canada</h1>
+                            <h1 className='text-4xl text-white font-bold font-mono p-4'>Data Analyst</h1>
+                            <h3 className='text-2xl text-white font-mono p-4'>March 2021 - August 2021</h3>
+                        </div>
+                    </div>
+                    <div className='text-center col-span-2 justify-center align-middle m-auto'>
+                        <ul className='text-white font-mono p-4 text-left'>
                             <li>• Was at the forefront of stakeholder outreach for Canada's COVID-19 testing program,
 dealing with small businesses, NGOs, and federal, provincial and municipal government
 officials.</li>
@@ -81,8 +62,19 @@ contagion across geographic areas.</li>
 integrity and successful data pipelining from a public frontend.</li>
                         </ul>
                     </div>
-                    <div className='text-left'>
-                        <ul className='text-white font-mono p-4'>
+                    <div className=''>
+                        <Image src={gov} alt="Government of Canada" width={500} height={500} className='rounded-full'/>
+                    </div>
+                    <div className='col-span-3 text-center'>
+                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Ottawa Hospital Research Institute</h1>
+                        <h1 className='text-4xl text-white font-bold font-mono p-4'>Bioinformatics Intern</h1>
+                        <h3 className='text-2xl text-white font-mono p-4'>May 2020 - August 2020</h3>
+                    </div>
+                    <div className=''>
+                        <Image src={hospital} alt="ohri" width={500} height={500} className='rounded-full'/>
+                    </div>
+                    <div className='text-center col-span-2 justify-center align-middle m-auto'>
+                        <ul className='text-white font-mono p-4 text-left'>
                             <li> Worked with a multi-disciplinary team to develop a data analysis pipeline for bulk RNA-seq data for Genome Canada's Genomic Applications Partnership Program (GAPP).</li>
                             <br></br>
                             <li>• Used a high performance CentOS cluster to schedule and test fusion callers using bash
