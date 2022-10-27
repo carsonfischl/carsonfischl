8ddb23c46b43f0362fc3be6e7fa9a5271a533fed
carsonfischl <carson.fischl@carleton.ca>
2022-10-27 12:19:38 -0400

    removed blog listings until posts are made

--- public/json/blogposts.js
+++ public/json/blogposts.js
@@ -1,23 +1,3 @@
 export const blogposts = 
 [ 
-    {
-        id: 1,
-        title: "My First Blog Post",
-        date: "2019-01-01",
-        firstLetter: "M",
-        tags: ["tag1", "tag2", "tag3"],
-        description: "This is my first blog post. It's about nothing in particular.",
-        href: "/blog/blogpost/1",
-        key: "blogpost1"
-    },
-    {
-        id: 2,
-        title: "My Second Blog Post",
-        date: "2019-01-02",
-        firstLetter: "M",
-        tags: ["tag1", "tag2", "tag3"],
-        description: "This is my second blog post. It's about nothing in particular.",
-        href: "/blog/blogpost/2",
-        key: "blogpost2"
-    },
 ]
