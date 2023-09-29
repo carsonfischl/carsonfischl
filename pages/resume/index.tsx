4c8548578a71f4207830dfe9443621bedf75d0a3
carsonfischl <carson.fischl@carleton.ca>
2023-09-29 14:35:01 +0200

    center attempt on CV

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -10,8 +10,8 @@ const index = (props: Props) => {
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
         <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-100 h-screen table-cell align-middle text-align items-center font-mono overscroll-contain content-center'>
             <Nav />
-            <div className="self-center justify-center align-middle w-100">
-              <iframe src="https://resume.creddle.io/embed/j47o66f6m78" width="827" height="1168" seamless className='self-center align-middle justify-center p-12 w-100'></iframe>
+            <div className="self-center justify-center align-middle w-100 grid">
+              <iframe src="https://resume.creddle.io/embed/j47o66f6m78" width="827" height="1168" seamless className='self-center align-middle justify-center w-100'></iframe>
             </div>
         </div>
     </>
