995cd5ca89f34aba949c7e25ce4fc2e038b5c2e9
carsonfischl <carson.fischl@carleton.ca>
2023-10-10 11:00:28 +0200

    updated work page with dell/IAE

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -7,6 +7,8 @@ import gov from '../../public/images/gov.png'
 import hospital from '../../public/images/hospital.png'
 import snps from '../../public/images/snps.png'
 import Image from 'next/image'
+import bordeaux from '../../public/images/IMG_1076.png'
+import dell from '../../public/images/IMG_7395.png'
 
 
 type Props = {}
@@ -19,6 +21,42 @@ const index = (props: Props) => {
         <Nav />
             <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100 overflow-y-hidden'>
                 <div className="grid grid-cols-3 gap-8 py-8">
+                <div className='col-span-3'>
+                        <div className='text-center'>
+                            <h1 className='text-4xl text-white font-bold font-mono p-4'>IAE Bordeaux / Université de Bordeaux</h1>
+                            <h1 className='text-4xl text-white font-bold font-mono p-4'>Graduate Teaching Assistant / Student Representative</h1>
+                            <h3 className='text-2xl text-white font-mono p-4'>September 2023 - Present</h3>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <Image src={bordeaux} alt="bordeaux" width={500} height={500} className='rounded-full'/>
+                    </div>
+                    <div className='text-center col-span-2 justify-center align-middle m-auto'>
+                        <ul className='text-white font-mono p-4 text-left'>
+                            <li>• Student representative of the university in class and at extracurricular events.</li>
+                            <br></br>
+                            <li>• Successfully managed simultaneous planning of student events, IT support for university staff, and rollout of new student attendance tracking system Edusign.</li>
+                        </ul>
+                    </div>
+                <div className='col-span-3'>
+                        <div className='text-center'>
+                            <h1 className='text-4xl text-white font-bold font-mono p-4'>Dell Technologies</h1>
+                            <h1 className='text-4xl text-white font-bold font-mono p-4'>Software Engineer I</h1>
+                            <h3 className='text-2xl text-white font-mono p-4'>January 2023 - September 2023</h3>
+                        </div>
+                    </div>
+                    <div className='text-center col-span-2 justify-center align-middle m-auto'>
+                        <ul className='text-white font-mono p-4 text-left'>
+                            <li>• Developed a multithreaded test framework using Python for the DU portion of Dell's 5G cloud RAN infrastructure.</li>
+                            <br></br>
+                            <li>• Assisted in Dell's successful launch into the 5G ORAN arena at Mobile World Congress 2023. </li>
+                            <br></br>
+                            <li>• Added LCOV reporting, pause/resume TTI feature, and other enhancements to a complex devops pipeline using Jenkins and GH integrations.</li>
+                        </ul>
+                    </div>
+                    <div className=''>
+                        <Image src={dell} alt="Dell" width={500} height={500} className='rounded-full'/>
+                    </div>
                     <div className='col-span-3'>
                         <div className='text-center'>
                             <h1 className='text-4xl text-white font-bold font-mono p-4'>Synopsys Inc.</h1>
