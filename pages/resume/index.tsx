659ea628834548a5b836b7e152aac4bb7d5d9866
carsonfischl <carson.fischl@carleton.ca>
2023-10-12 09:05:45 +0200

    fixed mobile formatting on resume page

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -8,7 +8,7 @@ const index = (props: Props) => {
   return (
     <>
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
-        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-100 h-screen table-cell align-middle text-align items-center font-mono overscroll-contain content-center'>
+        <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-screen h-screen table-cell align-middle text-align items-center font-mono fixed'>
             <Nav />
             <div className="self-center justify-center align-middle w-100 grid pb-12 pt-6">
               <iframe src="https://resume.creddle.io/embed/j47o66f6m78" width="827" height="1168" seamless className='self-center align-middle justify-center w-100'></iframe>
