bc428b05eb2be162d711b624697dce3cf6f6ee3a
carsonfischl <carson.fischl@carleton.ca>
2024-02-03 13:13:18 +0100

    Added favicon

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -9,7 +9,7 @@ const Meta = (props: Props) => {
         <Head>
             <title>{props.title}</title>
             <meta name="description" content={props.description} />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/icon.ico" />
         </Head>
     </div>
   )
