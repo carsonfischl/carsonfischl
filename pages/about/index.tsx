93eb15f3f63607b0f2eafb80f78fdfa46a3f2c36
carsonfischl <carson.fischl@carleton.ca>
2024-02-26 09:17:08 +0100

    pushed columns to about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -12,14 +12,22 @@ const About = (props: Props) => {
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
         <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-100 h-screen table-cell align-middle text-align items-center font-mono overscroll-contain'>
             <Nav/>
-            <div className='align-middle justify-center items-center justify-items-center w-4/5 m-auto h-100'>
-                <div className="items-left flex">
-                    <h1 className='text-4xl text-white font-bold font-mono p-4'>Hi!</h1>
-                    <h2 className='text-2xl text-white font-bold font-mono p-4'>My name is Carson.</h2>
+            <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100'>
+                <div className="grid grid-cols-3 gap-6 py-8">
+                    <div className='justify-centre align-middle m-auto'>
+                        <Image src={} alt='travel blog' className='rounded'/>
+                    </div>
+                    <h1 className='col-span-2 align-middle text-center justify-center m-auto py-8 text-2xl'>Hi! My name is Carson</h1>
+                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
+                        <h3 className='font-mono text-white text-2xl my-4'></h3>
+                    </div>
+                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
+                        <h3 className='font-mono text-white text-2xl my-4'>A Global Historiography through Numismatics</h3>
+                    </div>
+                    <div className='justify-centre align-middle m-auto'>
+                        <Image src={} alt='' className='rounded'/>
+                    </div>
                 </div>
-                <p className='font-mono text-white text-xl my-4'>I am a programmer, history buff, numismatist, traveler, and fitness enthusiast</p>
-                <p className='font-mono text-white text-xl my-4'>I am curretnly a graduate student at the University of Bordeaux, studying for my MBA. I am passionate about economics, digital marketing, and the synergies between information technology and business.</p>
-                <Image src={bordeaux} alt="bordeaux" width={500} height={500}/>
             </div>
         </div>
     </div>
