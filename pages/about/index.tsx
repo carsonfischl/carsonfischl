ae78e8608908b5173de6aa726f5dc236b0bcac54
carsonfischl <carson.fischl@carleton.ca>
2024-02-26 09:28:05 +0100

    return to previous design

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -12,52 +12,14 @@ const About = (props: Props) => {
         <Meta title='Carson S. Fischl' description='Scientist. Hacker. All-Around Eccentric.'/>
         <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-100 h-screen table-cell align-middle text-align items-center font-mono overscroll-contain'>
             <Nav/>
-            <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100'>
-                <div className="grid grid-cols-3 gap-6 py-8">
-                    <div className='justify-centre align-middle m-auto'>
-                        <Image src={bordeaux} alt='travel blog' className='rounded'/>
-                    </div>
-                    <h1 className='col-span-2 align-middle text-center justify-center m-auto py-8 text-2xl'>Hi! My name is Carson.</h1>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>Languages</h3>
-                        <Image src="https://skillicons.dev/icons?i=py,js,html,css,c,cpp,java,haskell,r" alt='travel blog' className='rounded'/>
-                    </div> 
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>Frameworks</h3>
-                        <Image src="https://skillicons.dev/icons?i=electron,flask,nextjs,qt,react" alt='travel blog' className='rounded'/>
-                    </div>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>Databases</h3>
-                        <Image src="https://skillicons.dev/icons?i=mongodb,mysql,sqlite" alt='travel blog' className='rounded'/>
-                    </div>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>Version control</h3>
-                        <Image src="https://skillicons.dev/icons?i=github,git,gitlab" alt='travel blog' className='rounded'/>
-                    </div>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>Ancillary tools</h3>
-                        <Image src="https://skillicons.dev/icons?i=bootstrap,latex,nginx,nodejs,postman,raspberrypi,selenium,svg,tensorflow,vercel,vim,vscode" alt='travel blog' className='rounded'/>
-                    </div>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>I can be found on</h3>
-                        <Image src="https://skillicons.dev/icons?i=gmail,instagram,discord,linkedin,stackoverflow,twitter" alt='travel blog' className='rounded'/>
-                    </div>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>Stats</h3>
-                        <Image src="https://github-readme-stats.vercel.app/api?username=carsonfischl&rank_icon=github" alt='travel blog' className='rounded'/>
-                    </div>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>Trophies</h3>
-                        <Image src="https://github-readme-stats.vercel.app/api/top-langs/?username=carsonfischl&hide=html,css,scss" alt='travel blog' className='rounded'/>
-                    </div>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
-                        <h3 className='font-mono text-white text-2xl my-4'>Leetcode</h3>
-                        <Image src="https://leetcard.jacoblin.cool/carson6f" alt='travel blog' className='rounded'/>
-                    </div>
-                    <div className='justify-centre align-middle m-auto'>
-                        <Image src={bordeaux} alt='' className='rounded'/>
-                    </div>
+            <div className='align-middle justify-center items-center justify-items-center w-4/5 m-auto h-100'>
+                <div className="items-left flex">
+                    <h1 className='text-4xl text-white font-bold font-mono p-4'>Hi!</h1>
+                    <h2 className='text-2xl text-white font-bold font-mono p-4'>My name is Carson.</h2>
                 </div>
+                <p className='font-mono text-white text-xl my-4'>I am a programmer, history buff, numismatist, traveler, and fitness enthusiast</p>
+                <p className='font-mono text-white text-xl my-4'>I am currently a graduate student at the University of Bordeaux, studying for my MBA. I am passionate about economics, digital marketing, and the synergies between information technology and business.</p>
+                <Image src={bordeaux} alt="bordeaux" width={500} height={500}/>
             </div>
         </div>
     </div>
