f43f9fb57120875eaa0bd1c99be699889a13cf44
carsonfischl <carson.fischl@carleton.ca>
2024-03-03 12:19:56 +0100

    changed to flexgrid on projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -17,7 +17,7 @@ const index = (props: Props) => {
         <div className='bg-gradient-to-r from-slate-500 to-slate-900 w-100 h-screen table-cell align-middle text-align items-center font-mono overscroll-contain'>
             <Nav />
             <div className='align-middle justify-center items-center flex justify-items-center w-4/5 m-auto h-100'>
-                <div className="grid grid-cols-3 gap-6 py-8">
+                <div className="flexgrid grid-cols-3 gap-6 py-8">
                     <div className='justify-centre align-middle m-auto'>
                         <Image src={nfa} alt='travel blog' className='rounded'/>
                     </div>
@@ -27,14 +27,14 @@ const index = (props: Props) => {
                         <br></br>
                         <a className='py-2 px-6 rounded-md hover:text-slate-800 bg-pink-500 font-mono text-white mx-2 my-8 ruby w-fit align-middle justify-center m-auto' href="https://no-fixed-address.netlify.app/">Under Construction</a>
                     </div>
-                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8'>
+                    <div className='justify-centre align-middle m-auto'>
+                        <Image src={cc} alt='Comitas Collection' className='rounded'/>
+                    </div>
+                    <div className='col-span-2 align-middle text-center justify-center m-auto py-8 mb-20'>
                         <h1 className='text-4xl text-white lg:font-bold font-mono p-4'>Comitas Collection</h1>
                         <h3 className='font-mono text-white text-2xl my-4'>A Global Historiography through Numismatics</h3>
                         <br></br>
                         <a className='py-2 px-6 rounded-md hover:text-slate-700 bg-pink-500 font-mono text-white mx-2 my-8 ruby' href="https://comitas-collection.vercel.app">Check it out</a>
-                    </div>
-                    <div className='justify-centre align-middle m-auto mb-20'>
-                        <Image src={cc} alt='Comitas Collection' className='rounded'/>
                     </div>
                 </div>
             </div>
